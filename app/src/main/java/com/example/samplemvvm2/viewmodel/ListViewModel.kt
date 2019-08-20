a216c2c52d936c0498eb2d70825b52baf957850b
Joko Priyono <joko.priyono@1rstwap.com>
2019-08-20 11:53:03 +0700

    Import dependecies and adding a view model

--- app/src/main/java/com/example/samplemvvm2/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/example/samplemvvm2/viewmodel/ListViewModel.kt
@@ -1,7 +1,33 @@
 package com.example.samplemvvm2.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.samplemvvm2.model.County
 
 class ListViewModel : ViewModel() {
+    val countries = MutableLiveData<List<County>>()
+    val countyLoadError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
 
+    fun refresh() {
+        fetchCountries()
+    }
+
+    private fun fetchCountries() {
+        val mockData = listOf(
+            County("CountryA"),
+            County("CountryB"),
+            County("CountryC"),
+            County("CountryD"),
+            County("CountryE"),
+            County("CountryF"),
+            County("CountryG"),
+            County("CountryH"),
+            County("CountryI"),
+            County("CountryJ")
+        )
+        countyLoadError.value = false
+        loading.value = false
+        countries.value = mockData
+    }
 }
